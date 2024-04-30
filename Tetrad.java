8b8af950c00295532c58234d607f832a0aa19cf3
kvine11 <kevinliu@team2714.com>
2024-04-30 09:05:48 -0500

    added cases in step 4 and partially finished step 5

--- Tetrad.java
+++ Tetrad.java
@@ -19,16 +19,98 @@ public class Tetrad
     // Constructs a Tetrad.
     public Tetrad(BoundedGrid<Block> grid)
     {
-        blocks = new Block[4];
-        Color color;
-        Location [] locs = {new Location(0,3), new Location(0,4), new Location(0,5), new Location(0,6)};
-        color = Color.RED;
-        for(int i = 0; i < blocks.length; i++)
+        int num = (int)(Math.random() * 7) + 0;
+        if(num == 0)
         {
-            blocks[i] = new Block();
-            blocks[i].setColor(color);
+            blocks = new Block[4];
+            Color color;
+            Location [] locs = {new Location(0,3), new Location(0,4), new Location(0,5), new Location(0,6)};
+            color = Color.RED;
+            for(int i = 0; i < blocks.length; i++)
+            {
+                blocks[i] = new Block();
+                blocks[i].setColor(color);
+            }
+            addToLocations(grid, locs);
         }
-        addToLocations(grid, locs);
+        else if(num == 1)
+        {
+            blocks = new Block[4];
+            Color color;
+            Location [] locs = {new Location(0,3), new Location(0,4), new Location(0,5), new Location(1,4)};
+            color = Color.GRAY;
+            for(int i = 0; i < blocks.length; i++)
+            {
+                blocks[i] = new Block();
+                blocks[i].setColor(color);
+            }
+            addToLocations(grid, locs);
+        }
+        else if(num == 2)
+        {
+            blocks = new Block[4];
+            Color color;
+            Location [] locs = {new Location(0,3), new Location(0,4), new Location(1,3), new Location(1,4)};
+            color = Color.CYAN;
+            for(int i = 0; i < blocks.length; i++)
+            {
+                blocks[i] = new Block();
+                blocks[i].setColor(color);
+            }
+            addToLocations(grid, locs);
+        }
+        else if(num == 3)
+        {
+            blocks = new Block[4];
+            Color color;
+            Location [] locs = {new Location(0,3), new Location(0,4), new Location(0,5), new Location(1,3)};
+            color = Color.YELLOW;
+            for(int i = 0; i < blocks.length; i++)
+            {
+                blocks[i] = new Block();
+                blocks[i].setColor(color);
+            }
+            addToLocations(grid, locs);
+        }
+        else if(num == 4)
+        {
+            blocks = new Block[4];
+            Color color;
+            Location [] locs = {new Location(0,3), new Location(0,4), new Location(0,5), new Location(1,5)};
+            color = Color.MAGENTA;
+            for(int i = 0; i < blocks.length; i++)
+            {
+                blocks[i] = new Block();
+                blocks[i].setColor(color);
+            }
+            addToLocations(grid, locs);
+        }
+        else if(num == 5)
+        {
+            blocks = new Block[4];
+            Color color;
+            Location [] locs = {new Location(0,4), new Location(0,5), new Location(1,3), new Location(1,4)};
+            color = Color.BLUE;
+            for(int i = 0; i < blocks.length; i++)
+            {
+                blocks[i] = new Block();
+                blocks[i].setColor(color);
+            }
+            addToLocations(grid, locs);
+        }
+        else
+        {
+            blocks = new Block[4];
+            Color color;
+            Location [] locs = {new Location(0,3), new Location(0,4), new Location(1,4), new Location(1,5)};
+            color = Color.GREEN;
+            for(int i = 0; i < blocks.length; i++)
+            {
+                blocks[i] = new Block();
+                blocks[i].setColor(color);
+            }
+            addToLocations(grid, locs);
+        }
     }
 
 
@@ -67,13 +149,26 @@ public class Tetrad
     //                and their old locations returned.
     private Location[] removeBlocks()
     {
-        throw new RuntimeException("INSERT MISSING CODE HERE");
+        Location[] revisedBlocks = new Location[blocks.length];
+        for(int i = 0; i < blocks.length; i++)
+        {
+            revisedBlocks[i] = blocks[i].getLocation();
+            blocks[i].removeSelfFromGrid();
+        }
+        return revisedBlocks;
     }
 
     // Postcondition: Returns true if each of the elements of locs is valid
     //                and empty in grid; false otherwise.
     private boolean areEmpty(BoundedGrid<Block> grid, Location[] locs)
     {
-        throw new RuntimeException("INSERT MISSING CODE HERE");
+        for(int i = 0; i < locs.length; i++)
+        {
+            if(grid.isValid(locs[i]) && grid.get(locs[i]) == null)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
