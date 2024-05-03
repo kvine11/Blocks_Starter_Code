b754f5c24b766347b8c8d8702ffe75c2ba868ee4
kvine11 <kevinliu@team2714.com>
2024-05-02 20:49:13 -0500

    finished step 12 (dont know if work). step 11 still owrking

--- Tetris.java
+++ Tetris.java
@@ -4,22 +4,18 @@
  * @author - Jane Doe
  * @author - Period n
  * @author - Id nnnnnnn
- *
  * @author - I received help from ...
- *
  */
 
 // Represents a Tetris game.
-public class Tetris implements ArrowListener
-{
-    private BoundedGrid<Block> grid;	// The grid containing the Tetris pieces.
-    private BlockDisplay display;		// Displays the grid.
-    private Tetrad activeTetrad;		// The active Tetrad (Tetris Piece).
+public class Tetris implements ArrowListener {
+    private BoundedGrid<Block> grid;    // The grid containing the Tetris pieces.
+    private BlockDisplay display;        // Displays the grid.
+    private Tetrad activeTetrad;        // The active Tetrad (Tetris Piece).
 
     // Constructs a Tetris Game
-    public Tetris()
-    {
-        grid = new BoundedGrid<Block>(20,10);
+    public Tetris() {
+        grid = new BoundedGrid<Block>(20, 10);
         display = new BlockDisplay(grid);
         display.setTitle("Tetris");
         display.setArrowListener(this);
@@ -28,23 +24,36 @@ public class Tetris implements ArrowListener
     }
 
     // Play the Tetris Game
-    public void play()
-    {
-        boolean play = true;
-        while(activeTetrad.translate(1,0))
+    public void play() {
+        while(true)
         {
-            sleep(1);
-            display.showBlocks();
+            while(activeTetrad.translate(1,0))
+            {
+                sleep(1);
+                display.showBlocks();
+            }
+            activeTetrad = new Tetrad(grid);
         }
+
+
+
+
     }
 
 
     // Precondition:  0 <= row < number of rows
     // Postcondition: Returns true if every cell in the given row
     //                is occupied; false otherwise.
-    private boolean isCompletedRow(int row)
-    {
-        throw new RuntimeException("INSERT MISSING CODE HERE");
+    private boolean isCompletedRow(int row) {
+        for(int i = 0; i < grid.getNumCols(); i++)
+        {
+            Location cell = new Location(row, i);
+            if(grid.get(cell) == null)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     // Precondition:  0 <= row < number of rows;
@@ -53,36 +62,56 @@ public class Tetris implements ArrowListener
     //                every block above row has been moved down one row.
     private void clearRow(int row)
     {
-        throw new RuntimeException("INSERT MISSING CODE HERE");
+        for(int i = 0; i < grid.getNumCols(); i++)
+        {
+            Location removeCell = new Location(row, i);
+            Block block = grid.get(removeCell);
+            block.removeSelfFromGrid();
+        }
+
+        for(int i = row - 1; i >= 0; i--)
+        {
+            for(int j = 0; j < grid.getNumCols(); j++)
+            {
+                Location oldLoc = new Location(i, j);
+                if(grid.get(oldLoc)!= null)
+                {
+                    Location shift = new Location(i + 1, j);
+                    grid.get(oldLoc).moveTo(shift);
+                }
+
+            }
+        }
+
     }
 
     // Postcondition: All completed rows have been cleared.
-    private void clearCompletedRows()
-    {
-        throw new RuntimeException("INSERT MISSING CODE HERE");
+    private void clearCompletedRows() {
+        for(int i = 0; i < grid.getNumRows(); i++)
+        {
+            if(isCompletedRow(i))
+            {
+                clearRow(i);
+            }
+        }
     }
 
     // Sleeps (suspends the active thread) for duration seconds.
-    private void sleep(double duration)
-    {
+    private void sleep(double duration) {
         final int MILLISECONDS_PER_SECOND = 1000;
 
-        int milliseconds = (int)(duration * MILLISECONDS_PER_SECOND);
+        int milliseconds = (int) (duration * MILLISECONDS_PER_SECOND);
 
-        try
-        {
+        try {
             Thread.sleep(milliseconds);
-        }
-        catch (InterruptedException e)
-        {
+        } catch (InterruptedException e) {
             System.err.println("Can't sleep!");
         }
     }
 
 
     // Creates and plays the Tetris game.
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Tetris game = new Tetris();
         game.play();
     }
@@ -95,20 +124,20 @@ public class Tetris implements ArrowListener
 
     @Override
     public void downPressed() {
-        activeTetrad.translate(1,0);
+        activeTetrad.translate(1, 0);
         display.showBlocks();
     }
 
     @Override
     public void leftPressed() {
-        activeTetrad.translate(0,-1);
+        activeTetrad.translate(0, -1);
         display.showBlocks();
 
     }
 
     @Override
     public void rightPressed() {
-        activeTetrad.translate(0,1);
+        activeTetrad.translate(0, 1);
         display.showBlocks();
 
     }
